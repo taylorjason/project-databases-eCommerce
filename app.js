f83f0c64f489eca504c1f0de82164dcab2b2e746
Jason Taylor <taylorjason@users.noreply.github.com>
2020-06-26 13:21:03 -0400

    Add testing file and user CRUDL operations in App.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const Pool = require('pg').Pool;
 const connection = new Pool({
   user: 'dev',
   host: 'localhost',
-  database: 'students',
+  database: 'ecommerce-project',
   password: 'dev-password',
   port: 5432
 });
@@ -17,18 +17,90 @@ app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/students', (req, res) => {
+
+// USERS
+// http://localhost:3000/users/?search=Jason
+// http://localhost:3000/users/
+
+app.get('/users/', (req, res) => {
   let search = req.query.search ? `%${req.query.search}%` : '%';
   connection.query(
-    'SELECT * FROM students WHERE first_name LIKE $1',
-    [search],
+    'SELECT * FROM users WHERE first_name ILIKE $1 OR last_name ILIKE $2',
+    [search, search],
     (error, results) => {
       if (error) {
-        throw error;
+        res.send(error.message);
+      } else {
+        res.send(JSON.stringify(results.rows));
       }
-      res.send(JSON.stringify(results.rows));
     }
   );
 });
+
+/**
+ *
+ */
+
+app.patch('/users/:userID', (req, res) => {
+  let update = req.body;
+  let updateStr = [];
+  let id = req.params.userID;
+
+  for (let key in update) {
+    updateStr.push(`${key} = '${update[key]}'`);
+  }
+  connection.query(
+    `UPDATE users SET ${updateStr.join(', ')} WHERE id = $1 RETURNING *`,
+    [id],
+    (error, results) => {
+      if (error) {
+        res.send(error.message);
+      } else {
+        res.send(JSON.stringify(results.rows));
+      }
+    }
+  );
+});
+
+app.delete('/users/:userID', (req, res) => {
+  let deleteUser = req.params.userID;
+
+  connection.query(
+    'DELETE FROM users WHERE id = $1 RETURNING *',
+    [deleteUser],
+    (error, results) => {
+      if (error) {
+        res.send(error.message);
+      } else {
+        res.send(JSON.stringify(results.rows));
+      }
+    }
+  );
+});
+
+app.post('/users/', (req, res) => {
+  let newUser = req.body;
+  let newStr = [];
+
+  console.log(req.body);
+  for (let key in newUser) {
+    newStr.push(`'${newUser[key]}'`);
+  }
+  console.log(newStr.join(', '));
+
+  let pass = '(' + newStr.join(', ') + ')';
+  connection.query(
+    `INSERT INTO users (first_name, last_name, email) VALUES ${pass} RETURNING *`,
+    (error, results) => {
+      if (error) {
+        res.send(error);
+      } else {
+        res.send(JSON.stringify(results.rows));
+      }
+    }
+  );
+});
+
+
 
 app.listen(port, () => console.log('listening on port 3000'));
---
+++ itemsQueries.js
@@ -0,0 +1,1 @@
+itemsQueries.js;
