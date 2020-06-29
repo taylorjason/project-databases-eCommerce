cd485c550eb68ea61d60dbcfef33d562caef8464
Jason Taylor <taylorjason@users.noreply.github.com>
2020-06-29 18:30:15 -0400

    clean up after figurign out rendering issue

--- ecommercefrontend/src/components/Users.js
+++ ecommercefrontend/src/components/Users.js
@@ -6,23 +6,23 @@ function Users({ id }) {
   useEffect(() => {
     (async () => {
       const result = await fetch(`http://localhost:3001/users/`);
-
-      await console.log('running useEffect');
-      //   if (!result.ok) return;
+      if (!result.ok) return;
 
       const jsonResult = await result.json();
-      console.log(jsonResult);
-
       setUserData(jsonResult);
     })();
   }, []);
 
   return (
     <div>
-      User Page {console.log('userData is', userData)}
+      Current Users {console.log('userData is', userData)}
       <ul>
         {userData.map((user) => (
-          <li>{user.first_name ? user.first_name : 'loading...'}</li>
+          <li key={user.id}>
+            {user.first_name
+              ? user.first_name + ' ' + user.last_name
+              : 'loading...'}
+          </li>
         ))}
       </ul>
     </div>
